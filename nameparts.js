8381d2dc442d2fec0657ac0504c82ce4eeff31b2
Henry Navntoft <henrylnavntoft@gmail.com>
2023-02-06 15:42:16 +0100

    change

--- nameparts.js
+++ nameparts.js
@@ -1,9 +1,13 @@
+"use strict";
+
 const fullName = "Henry Lundberg Navntoft";
 
+const lastSpaceIndex = fullName.lastIndexOf(" ");
+
 const firstSpaceIndex = fullName.indexOf(" ");
+
 const firstName = fullName.substring(0, firstSpaceIndex);
 
-const lastSpaceIndex = fullName.lastIndexOf(" ");
 const lastName = fullName.substring(lastSpaceIndex + 1);
 
 const middleName = fullName.substring(firstSpaceIndex + 1, lastSpaceIndex);
@@ -12,10 +16,6 @@ console.log(fullName);
 
 console.log(firstName);
 
-console.log(firstSpaceIndex);
-
 console.log(middleName);
 
 console.log(lastName);
-
-console.log(lastSpaceIndex);
