cc63a83544a095ccb6b1584ca1cc5ae3920de261
Henry Navntoft <henrylnavntoft@gmail.com>
2023-02-08 11:24:48 +0100

    counting

--- counting_arrays.js
+++ counting_arrays.js
@@ -1,3 +1,18 @@
 "use strict";
+let count = 0;
+let array = [];
 
-console.log("start");
+function startCounting() {
+  array.unshift(count);
+  if (array.length > 9) {
+    array.pop();
+  }
+  console.clear();
+  console.log(array);
+  count++;
+  if (count < 50) {
+    setTimeout(startCounting, 400);
+  }
+}
+
+startCounting();
